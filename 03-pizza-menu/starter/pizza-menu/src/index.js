1965c55dbfe8bfeee7bb9c141cfe49d3a1f68f82
Nino Akut <akutnino@gmail.com>
2024-05-14 00:35:16 +0800

    reviewed about React Fragments

--- 03-pizza-menu/starter/pizza-menu/src/index.js
+++ 03-pizza-menu/starter/pizza-menu/src/index.js
@@ -74,14 +74,21 @@ function Menu(props) {
 			<h2>Our Menu</h2>
 
 			{isPizzasInStock ? (
-				<ul className='pizzas'>
-					{pizzas.map((pizzaObject, index) => (
-						<Pizza
-							pizzaObject={pizzaObject}
-							key={index}
-						/>
-					))}
-				</ul>
+				<React.Fragment>
+					<p>
+						Authentic Italian cuisine. 6 creative dishes to choose from. All
+						from our stone oven, all organic, all delicious.
+					</p>
+
+					<ul className='pizzas'>
+						{pizzas.map((pizzaObject, index) => (
+							<Pizza
+								pizzaObject={pizzaObject}
+								key={index}
+							/>
+						))}
+					</ul>
+				</React.Fragment>
 			) : (
 				<p>We're still working on our menu. Please come back later.</p>
 			)}
