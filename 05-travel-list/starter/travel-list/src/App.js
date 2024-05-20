bf71c4acc74663c3091ff5a203732fc47db7633b
Nino Akut <akutnino@gmail.com>
2024-05-21 02:02:51 +0800

    revised Calculating Statistics as Derived State

--- 05-travel-list/starter/travel-list/src/App.js
+++ 05-travel-list/starter/travel-list/src/App.js
@@ -139,20 +139,24 @@ function ListItem(props) {
 function Stats(props) {
 	const { itemsArray } = props;
 	const totalItems = itemsArray.length;
-	const totalItemsPacked = itemsArray.filter((itemObject) =>
-		itemObject.packed ? true : false
-	).length;
+	const totalItemsPacked = itemsArray.reduce(
+		(total, itemObject) => (itemObject.packed ? (total += 1) : (total += 0)),
+		0
+	);
 	const percentage = Number((totalItemsPacked / totalItems) * 100).toFixed(0);
 	const packedItemsPercentage = percentage === 'NaN' ? 0 : Number(percentage);
 
 	return (
 		<footer className='stats'>
-			<em>
-				{packedItemsPercentage === 100
-					? `You are raedy to Go!`
-					: `💼 You have ${totalItems} items on your list, 
-							and you already packed ${totalItemsPacked} (${packedItemsPercentage}%)`}
-			</em>
+			{totalItems === 0 && <em>Start adding things to the list!</em>}
+			{totalItems !== 0 && (
+				<em>
+					{packedItemsPercentage === 100
+						? 'You are ready to Go!'
+						: `You have ${totalItems} items on your list, and you already packed 
+						${totalItemsPacked} (${packedItemsPercentage}%)`}
+				</em>
+			)}
 		</footer>
 	);
 }
