ab96cbd2e14f5bc5617319568a8816058b4796e3
Nino Akut <akutnino@gmail.com>
2024-05-24 02:43:12 +0800

    created the FormSplitBill component

--- 06-eat-n-split/starter/eat-n-split/src/App.js
+++ 06-eat-n-split/starter/eat-n-split/src/App.js
@@ -27,6 +27,8 @@ export default function App(props) {
 				<FormAddFriend />
 				<Button>Add Friend</Button>
 			</div>
+
+			<FormSplitBill />
 		</div>
 	);
 }
@@ -96,6 +98,34 @@ function FormAddFriend(props) {
 			<input type='text' />
 
 			<Button>Add</Button>
+		</form>
+	);
+}
+
+function FormSplitBill(props) {
+	return (
+		<form className='form-split-bill'>
+			<h2>Split a bill with X</h2>
+
+			<label>💰 Bill value</label>
+			<input type='number' />
+
+			<label>🧍‍♂️ Your expense</label>
+			<input type='number' />
+
+			<label>🧑‍🤝‍👩 X's expense</label>
+			<input
+				type='number'
+				disabled
+			/>
+
+			<label>🤑 Who is paying the bill</label>
+			<select>
+				<option value='user'>You</option>
+				<option value='friend'>X</option>
+			</select>
+
+			<Button>Split Bill</Button>
 		</form>
 	);
 }
