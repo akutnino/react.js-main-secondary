f4ac9ac9e5fbaaa86da49743a3c9171eb19f9557
Nino Akut <akutnino@gmail.com>
2024-05-27 23:51:50 +0800

    abstracted the WatchedMoviesList to a separate component

--- 07-usepopcorn/starter/usepopcorn/src/App.js
+++ 07-usepopcorn/starter/usepopcorn/src/App.js
@@ -188,32 +188,7 @@ function WatchedBox(props) {
 			{isOpen2 && (
 				<>
 					<WatchedSummary watched={watched} />
-
-					<ul className='list'>
-						{watched.map((movie) => (
-							<li key={movie.imdbID}>
-								<img
-									src={movie.Poster}
-									alt={`${movie.Title} poster`}
-								/>
-								<h3>{movie.Title}</h3>
-								<div>
-									<p>
-										<span>⭐️</span>
-										<span>{movie.imdbRating}</span>
-									</p>
-									<p>
-										<span>🌟</span>
-										<span>{movie.userRating}</span>
-									</p>
-									<p>
-										<span>⏳</span>
-										<span>{movie.runtime} min</span>
-									</p>
-								</div>
-							</li>
-						))}
-					</ul>
+					<WatchedMoviesList watched={watched} />
 				</>
 			)}
 		</div>
@@ -248,5 +223,37 @@ function WatchedSummary(props) {
 				</p>
 			</div>
 		</div>
+	);
+}
+
+function WatchedMoviesList(props) {
+	const { watched } = props;
+
+	return (
+		<ul className='list'>
+			{watched.map((movie) => (
+				<li key={movie.imdbID}>
+					<img
+						src={movie.Poster}
+						alt={`${movie.Title} poster`}
+					/>
+					<h3>{movie.Title}</h3>
+					<div>
+						<p>
+							<span>⭐️</span>
+							<span>{movie.imdbRating}</span>
+						</p>
+						<p>
+							<span>🌟</span>
+							<span>{movie.userRating}</span>
+						</p>
+						<p>
+							<span>⏳</span>
+							<span>{movie.runtime} min</span>
+						</p>
+					</div>
+				</li>
+			))}
+		</ul>
 	);
 }
