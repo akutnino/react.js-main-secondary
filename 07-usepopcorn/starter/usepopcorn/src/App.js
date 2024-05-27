1142fe005f602d2d9f7d458321f56b624a55d506
Nino Akut <akutnino@gmail.com>
2024-05-27 23:56:52 +0800

    abstracted the WatchedMovieItem to a separate component

--- 07-usepopcorn/starter/usepopcorn/src/App.js
+++ 07-usepopcorn/starter/usepopcorn/src/App.js
@@ -232,28 +232,39 @@ function WatchedMoviesList(props) {
 	return (
 		<ul className='list'>
 			{watched.map((movie) => (
-				<li key={movie.imdbID}>
-					<img
-						src={movie.Poster}
-						alt={`${movie.Title} poster`}
-					/>
-					<h3>{movie.Title}</h3>
-					<div>
-						<p>
-							<span>⭐️</span>
-							<span>{movie.imdbRating}</span>
-						</p>
-						<p>
-							<span>🌟</span>
-							<span>{movie.userRating}</span>
-						</p>
-						<p>
-							<span>⏳</span>
-							<span>{movie.runtime} min</span>
-						</p>
-					</div>
-				</li>
+				<WatchedMovieItem
+					movie={movie}
+					key={movie.imdbID}
+				/>
 			))}
 		</ul>
+	);
+}
+
+function WatchedMovieItem(props) {
+	const { movie } = props;
+
+	return (
+		<li>
+			<img
+				src={movie.Poster}
+				alt={`${movie.Title} poster`}
+			/>
+			<h3>{movie.Title}</h3>
+			<div>
+				<p>
+					<span>⭐️</span>
+					<span>{movie.imdbRating}</span>
+				</p>
+				<p>
+					<span>🌟</span>
+					<span>{movie.userRating}</span>
+				</p>
+				<p>
+					<span>⏳</span>
+					<span>{movie.runtime} min</span>
+				</p>
+			</div>
+		</li>
 	);
 }
