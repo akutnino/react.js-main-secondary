6a1d6f783fc5c474531e757c25f7680bdc7fd4c5
Nino Akut <akutnino@gmail.com>
2024-05-29 00:41:21 +0800

    removed both ListBox and WatchedBox components

--- 07-usepopcorn/starter/usepopcorn/src/App.js
+++ 07-usepopcorn/starter/usepopcorn/src/App.js
@@ -148,23 +148,6 @@ function Box(props) {
 	);
 }
 
-function ListBox(props) {
-	const { movies } = props;
-	const [isOpen1, setIsOpen1] = useState(true);
-
-	return (
-		<div className='box'>
-			<button
-				className='btn-toggle'
-				onClick={() => setIsOpen1((open) => !open)}
-			>
-				{isOpen1 ? '–' : '+'}
-			</button>
-			{isOpen1 && <MovieList movies={movies} />}
-		</div>
-	);
-}
-
 function MovieList(props) {
 	const { movies } = props;
 
@@ -197,28 +180,6 @@ function MovieItem(props) {
 				</p>
 			</div>
 		</li>
-	);
-}
-
-function WatchedBox(props) {
-	const [watched, setWatched] = useState(tempWatchedData);
-	const [isOpen2, setIsOpen2] = useState(true);
-
-	return (
-		<div className='box'>
-			<button
-				className='btn-toggle'
-				onClick={() => setIsOpen2((open) => !open)}
-			>
-				{isOpen2 ? '–' : '+'}
-			</button>
-			{isOpen2 && (
-				<>
-					<WatchedSummary watched={watched} />
-					<WatchedMoviesList watched={watched} />
-				</>
-			)}
-		</div>
 	);
 }
 
