e189312f11b622fc53f3e7eb3e598caa377a3864
Nino Akut <akutnino@gmail.com>
2024-05-30 01:51:21 +0800

    created state for userRating and hoverRating for a dynamic component

--- 07-usepopcorn/starter/usepopcorn/src/StarRating.js
+++ 07-usepopcorn/starter/usepopcorn/src/StarRating.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const containerStyle = {
 	display: 'flex',
 	alignItems: 'center',
@@ -23,66 +25,75 @@ const starStyle = {
 
 export default function StarRating(props) {
 	const { maxRating = 5 } = props;
+	const [userRating, setUserRating] = useState(0);
+	const [hoverRating, setHoverRating] = useState(0);
 
+	const handleRating = (rating) => {
+		return () => setUserRating(rating);
+	};
+
+	const handleHoverEnter = (rating) => {
+		return () => setHoverRating(rating);
+	};
+
+	const handleHoverLeave = () => {
+		setHoverRating(0);
+	};
+
+	// prettier-ignore
 	return (
 		<div style={containerStyle}>
 			<div style={starContainerStyle}>
-				{Array.from(Array(maxRating)).map((val, index) => (
-					<Star key={index} />
+				{Array.from(Array(maxRating)).map((value, index) => (
+					<Star
+						key={index}
+						onClick={handleRating(index + 1)}
+						isStarUserRating={hoverRating >= index + 1 || userRating >= index + 1}
+						onMouseEnter={handleHoverEnter(index + 1)}
+						onMouseLeave={handleHoverLeave}
+					/>
 				))}
 			</div>
-			<p style={textStyle}>10</p>
+			<p style={textStyle}>{hoverRating || userRating || ''}</p>
 		</div>
 	);
 }
 
 function Star(props) {
+	const { onClick, isStarUserRating, onMouseEnter, onMouseLeave } = props;
+
 	return (
 		<span
-			role='button'
+			role={'button'}
 			style={starStyle}
+			onClick={onClick}
+			onMouseEnter={onMouseEnter}
+			onMouseLeave={onMouseLeave}
 		>
-			<svg
-				xmlns='http://www.w3.org/2000/svg'
-				viewBox='0 0 20 20'
-				fill='#000'
-				stroke='#000'
-			>
-				<path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
-			</svg>
+			{isStarUserRating ? (
+				<svg
+					xmlns='http://www.w3.org/2000/svg'
+					viewBox='0 0 20 20'
+					fill='#000'
+					stroke='#000'
+				>
+					<path d='M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z' />
+				</svg>
+			) : (
+				<svg
+					xmlns='http://www.w3.org/2000/svg'
+					fill='none'
+					viewBox='0 0 24 24'
+					stroke='#000'
+				>
+					<path
+						strokeLinecap='round'
+						strokeLinejoin='round'
+						strokeWidth='{2}'
+						d='M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z'
+					/>
+				</svg>
+			)}
 		</span>
 	);
 }
-
-/*
-FULL STAR
-
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  viewBox="0 0 20 20"
-  fill="#000"
-  stroke="#000"
->
-  <path
-    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-  />
-</svg>
-
-
-EMPTY STAR
-
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  fill="none"
-  viewBox="0 0 24 24"
-  stroke="#000"
->
-  <path
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    strokeWidth="{2}"
-    d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-  />
-</svg>
-
-*/
