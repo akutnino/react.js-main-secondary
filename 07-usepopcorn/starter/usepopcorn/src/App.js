da00af27786d2b601b475ecea0f2b0926a1a5c35
Nino Akut <akutnino@gmail.com>
2024-06-14 22:01:03 +0800

    reviewed The Rules of Hooks in Practice

--- 07-usepopcorn/starter/usepopcorn/src/App.js
+++ 07-usepopcorn/starter/usepopcorn/src/App.js
@@ -280,6 +280,9 @@ function MovieDetails(props) {
 		Director: director
 	} = selectedMovieObject;
 
+	/* eslint-disable */
+	if (imdbRating > 8) [isTop, setIsTop] = useState(true);
+
 	const handleCloseMovieDetails = () => {
 		setSelectedMovieId(null);
 		setSelectedMovieObject({});
