604d37d33b46ea6491fe63dd4727d94b1379bce1
Nino Akut <akutnino@gmail.com>
2024-06-19 22:16:52 +0800

    created and transferred SearchBar files and logic in component folder

--- 07-usepopcorn/starter/usepopcorn/src/components/App.js
+++ 07-usepopcorn/starter/usepopcorn/src/components/App.js
@@ -3,6 +3,7 @@ import { useMovies } from '../hooks/useMovies';
 import { useLocalStorageState } from '../hooks/useLocalStorageState';
 import StarRating from './StarRating';
 import NavBar from './NavBar';
+import SearchBar from './SearchBar';
 
 const KEY = '3494c38';
 
@@ -75,47 +76,6 @@ export default function App() {
 				</Box>
 			</Main>
 		</>
-	);
-}
-
-function SearchBar(props) {
-	const { query, setQuery, setSelectedMovieId, setSelectedMovieObject } = props;
-	const inputElement = useRef(null);
-
-	const handleSearchInput = (event) => {
-		setQuery(event.target.value);
-	};
-
-	useEffect(() => {
-		inputElement.current.focus();
-
-		const keypressCallback = (event) => {
-			if (document.activeElement === inputElement.current) return;
-
-			if (event.key === 'Enter') {
-				inputElement.current.focus();
-				setQuery('');
-				setSelectedMovieId(null);
-				setSelectedMovieObject({});
-			}
-		};
-
-		document.addEventListener('keypress', keypressCallback);
-
-		return () => {
-			document.removeEventListener('keypress', keypressCallback);
-		};
-	}, [setQuery, setSelectedMovieId, setSelectedMovieObject]);
-
-	return (
-		<input
-			className='search'
-			type='text'
-			placeholder='Search movies...'
-			value={query}
-			onChange={handleSearchInput}
-			ref={inputElement}
-		/>
 	);
 }
 
