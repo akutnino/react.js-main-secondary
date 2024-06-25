1e976d39c44248a49032b165f8edd4fbdc7afb57
Nino Akut <akutnino@gmail.com>
2024-06-26 00:34:46 +0800

    implemented the Header and Main components in App.js for react-quiz app

--- 10-react-quiz/starter/react-quiz/src/App.js
+++ 10-react-quiz/starter/react-quiz/src/App.js
@@ -1,3 +1,15 @@
+import Header from './Header';
+import Main from './Main';
+
 export default function App(props) {
-	return <div></div>;
+	return (
+		<div className='app'>
+			<Header />
+
+			<Main className='main'>
+				<p>1/15</p>
+				<p>Question</p>
+			</Main>
+		</div>
+	);
 }
