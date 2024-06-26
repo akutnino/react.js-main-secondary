8e79ac2bc0607222110b94b19e8c07ba9c409efc
Nino Akut <akutnino@gmail.com>
2024-06-26 17:10:19 +0800

    fetch data from api with useEffect

--- 10-react-quiz/starter/react-quiz/src/App.js
+++ 10-react-quiz/starter/react-quiz/src/App.js
@@ -1,7 +1,28 @@
+import { useEffect } from 'react';
 import Header from './Header';
 import Main from './Main';
 
 export default function App(props) {
+	useEffect(() => {
+		const fetchQuestions = async () => {
+			try {
+				const fetchURL = 'http://localhost:8000/questions';
+				const fetchOptions = {};
+
+				const response = await fetch(fetchURL, fetchOptions);
+				if (!response.ok) throw new Error('FETCH REQUEST FAILED');
+
+				const data = await response.json();
+				console.log(data);
+			} catch (error) {
+				console.error(error);
+			}
+		};
+
+		fetchQuestions();
+		return () => {};
+	}, []);
+
 	return (
 		<div className='app'>
 			<Header />
