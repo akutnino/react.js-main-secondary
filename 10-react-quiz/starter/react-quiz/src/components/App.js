2ec3478ee9df0fdf1b9cc99ec647be16e40f92a2
Nino Akut <akutnino@gmail.com>
2024-06-29 21:57:30 +0800

    minor format changes

--- 10-react-quiz/starter/react-quiz/src/components/App.js
+++ 10-react-quiz/starter/react-quiz/src/components/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useReducer, useRef } from 'react';
+import { useEffect, useReducer } from 'react';
 import Header from './Header';
 import Main from './Main';
 import Loader from './Loader';
