e93964915b12936570834ca4fa7261c3e9cc6440
Nino Akut <akutnino@gmail.com>
2024-07-04 02:26:02 +0800

    installed sass package and inserted nav tag in PageNav component

--- 11-worldwise/starter/worldwise/src/components/PageNav.jsx
+++ 11-worldwise/starter/worldwise/src/components/PageNav.jsx
@@ -2,16 +2,18 @@ import { NavLink } from 'react-router-dom';
 
 export default function PageNav() {
 	return (
-		<ul>
-			<li>
-				<NavLink to={'/'}>Home</NavLink>
-			</li>
-			<li>
-				<NavLink to={'/product'}>Product</NavLink>
-			</li>
-			<li>
-				<NavLink to={'/pricing'}>Pricing</NavLink>
-			</li>
-		</ul>
+		<nav>
+			<ul>
+				<li>
+					<NavLink to={'/'}>Home</NavLink>
+				</li>
+				<li>
+					<NavLink to={'/product'}>Product</NavLink>
+				</li>
+				<li>
+					<NavLink to={'/pricing'}>Pricing</NavLink>
+				</li>
+			</ul>
+		</nav>
 	);
 }
