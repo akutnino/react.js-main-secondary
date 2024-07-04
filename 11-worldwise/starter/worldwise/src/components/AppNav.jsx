3ce547b0e2c93447a12b3a445ab28cdb6096ba6f
Nino Akut <akutnino@gmail.com>
2024-07-04 22:16:57 +0800

    created AppNav page and scss file

---
+++ 11-worldwise/starter/worldwise/src/components/AppNav.jsx
@@ -0,0 +1,5 @@
+import styles from './AppNav.module.scss';
+
+export default function AppNav() {
+	return <nav className={styles.nav}>App Navigation</nav>;
+}
--- 11-worldwise/starter/worldwise/src/pages/Homepage.jsx
+++ 11-worldwise/starter/worldwise/src/pages/Homepage.jsx
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom';
 import PageNav from '../components/PageNav';
+import AppNav from '../components/AppNav';
 
 export default function Homepage() {
 	return (
 		<div>
 			<PageNav />
+			<AppNav />
 
 			<Link to={'/app'}>Go to App</Link>
 		</div>
