461341feaa51b97ba0e6c9c2c26f5024f4446dec
Nino Akut <akutnino@gmail.com>
2024-07-05 22:49:45 +0800

    added and modified all pages folder starter files

---
+++ 11-worldwise/starter/worldwise/src/pages/Login.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import styles from './Login.module.scss';
+
+export default function Login() {
+	// PRE-FILL FOR DEV PURPOSES
+	const [email, setEmail] = useState('jack@example.com');
+	const [password, setPassword] = useState('qwerty');
+
+	const handleUserEmail = (event) => {
+		setEmail(event.target.value);
+	};
+
+	const handleUserPassword = (event) => {
+		setPassword(event.target.value);
+	};
+
+	return (
+		<main className={styles.login}>
+			<form className={styles.form}>
+				<div className={styles.row}>
+					<label htmlFor='email'>Email address</label>
+					<input
+						type='email'
+						id='email'
+						onChange={handleUserEmail}
+						value={email}
+					/>
+				</div>
+
+				<div className={styles.row}>
+					<label htmlFor='password'>Password</label>
+					<input
+						type='password'
+						id='password'
+						onChange={handleUserPassword}
+						value={password}
+					/>
+				</div>
+
+				<div>
+					<button>Login</button>
+				</div>
+			</form>
+		</main>
+	);
+}
