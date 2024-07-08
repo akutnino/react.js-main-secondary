0ed40f14bed0af2d7342bdb2dc9f6d2a9be5c0da
Nino Akut <akutnino@gmail.com>
2024-07-09 00:13:10 +0800

    added nested routes in App.jsx and used the Outlet component

--- 11-worldwise/starter/worldwise/src/App.jsx
+++ 11-worldwise/starter/worldwise/src/App.jsx
@@ -29,7 +29,24 @@ export default function App() {
 				<Route
 					path='app'
 					element={<AppLayout />}
-				/>
+				>
+					<Route
+						index
+						element={<p>List of Cities Element</p>}
+					/>
+					<Route
+						path='cities'
+						element={<p>List of Cities Element</p>}
+					/>
+					<Route
+						path='countries'
+						element={<p>Countries</p>}
+					/>
+					<Route
+						path='form'
+						element={<p>Form</p>}
+					/>
+				</Route>
 				<Route
 					path='*'
 					element={<PageNotFound />}
--- 11-worldwise/starter/worldwise/src/components/Sidebar.jsx
+++ 11-worldwise/starter/worldwise/src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom';
 import styles from './../styles/Sidebar.module.scss';
 import AppNav from './AppNav';
 import Logo from './Logo';
@@ -8,7 +9,7 @@ export default function Sidebar() {
 			<Logo />
 			<AppNav />
 
-			<p>List of Cities</p>
+			<Outlet />
 
 			<footer className={styles.footer}>
 				&copy; Copyright {new Date().getFullYear()} by WorldWide Inc.
